e0c15e5987d4c82803975db6dd97cb9ac3d7a784
JunmingGuo <sjunmg@gmail.com>
2021-04-09 22:39:33 -0700

    Set up basic routing

--- github-user-search/src/App.tsx
+++ github-user-search/src/App.tsx
@@ -1,13 +1,22 @@
 import { FunctionComponent } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Dashboard, Login, Error } from './pages';
 
 const App: FunctionComponent = () => {
   return (
-    <div>
-      <Dashboard />
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
