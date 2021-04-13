f5c089c1a7880921914addd58c20efc3fd61c972
JunmingGuo <sjunmg@gmail.com>
2021-04-12 21:43:46 -0700

    Update Search component Add markup and style Toggle search button based on remaining requests

--- github-user-search/src/components/Search.tsx
+++ github-user-search/src/components/Search.tsx
@@ -1,8 +1,39 @@
+import { FormEvent, useContext, useState } from 'react';
 import styled from 'styled-components';
+import { MdSearch } from 'react-icons/md';
+import { GithubContext } from '../context/context';
 
 const Search = () => {
+  const [user, setUser] = useState('');
+  const { remainRequests } = useContext(GithubContext);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (user) {
+    }
+  };
+
   return (
-    <Wrapper>Search Component</Wrapper>
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={e => setUser(e.target.value)}
+            />
+            {remainRequests && remainRequests > 0 && (
+              <button type="submit">search</button>
+            )}
+          </div>
+        </form>
+        <h3>requests: {remainRequests}/60</h3>
+      </Wrapper>
+    </section>
   );
 };
 
--- github-user-search/src/context/context.tsx
+++ github-user-search/src/context/context.tsx
@@ -1,12 +1,14 @@
-import { createContext, FunctionComponent, useState } from 'react';
+import { createContext, FunctionComponent, useEffect, useState } from 'react';
+import axios from 'axios';
 import { mockUser, mockRepos, mockFollowers } from './mockData/mockData';
 
-// const rootUrl = 'https://api.github.com';
+const rootUrl = 'https://api.github.com';
 
 interface ContextProps {
   githubUser: any;
   repos: any[];
   followers: any[];
+  remainRequests: number;
 }
 
 const GithubContext = createContext<Partial<ContextProps>>({});
@@ -18,12 +20,34 @@ const GithubProvider: FunctionComponent = ({ children }) => {
   const [followers, setFollowers] = useState(mockFollowers);
   // eslint-disable-next-line
   const [repos, setRepos] = useState(mockRepos);
+  // Request, Loading
+  // eslint-disable-next-line
+  const [remainRequests, setRemainRequests] = useState(0);
+  // eslint-disable-next-line
+  const [isLoading, setIsLoading] = useState(false);
+  // Error
+
+  // Check rate
+  const checkRemainRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        console.log(data);
+        let { remaining } = data.rate;
+        setRemainRequests(remaining);
+        if (remaining === 0) {
+          // throw an error
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    checkRemainRequests();
+  }, []);
 
   return (
-    <GithubContext.Provider 
-      value={{githubUser, repos, followers}}
-    >
-      { children }
+    <GithubContext.Provider value={{ githubUser, repos, followers, remainRequests }}>
+      {children}
     </GithubContext.Provider>
   );
 };
