c6e260d0fa8a76ab14a0f71e2a0813310e2a66bd
JunmingGuo <sjunmg@gmail.com>
2021-04-13 16:32:45 -0700

    Add error catch

--- github-user-search/src/context/context.tsx
+++ github-user-search/src/context/context.tsx
@@ -50,7 +50,7 @@ const GithubProvider: FunctionComponent = ({ children }) => {
         if (followers.status === fulfilled) {
           setFollowers(followers.value.data);
         }
-      });
+      }).catch(err => console.log(err));
     } else {
       toggleError(true, 'there is no user with that username');
     }
